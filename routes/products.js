de070ebf59e8727ed2a181e7390b2bfcf6620f4a
DonRuan22 <ruan_donino@hotmail.com>
2021-09-22 11:02:20 -0300

    add search model

--- routes/products.js
+++ routes/products.js
@@ -180,7 +180,7 @@ router.get('/:prodId', async (req, res) => {
 });
 
 /* GET ONE PRODUCT*/
-router.get('/model/:prodModel', async (req, res) => {
+router.get('/model', async (req, res) => {
     let productModel = req.query.prodModel;
     /*
     database.table('products as p')
